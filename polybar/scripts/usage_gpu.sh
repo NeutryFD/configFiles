ff76031a984fe55d71cc642cf077354252403425
NeutryFD <luisangel20.las@gmail.com>
2025-03-29 12:00:16 +0100

    update VRAM usage script file name

--- polybar/scripts/usage_gpu.sh
+++ polybar/scripts/usage_gpu.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+
+memory_total=16376
+memory_used=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits)
 awk -v x="$memory_used" -v y="$memory_total" 'BEGIN { printf "GPU %.1f%",(x / y) * 100 }'
