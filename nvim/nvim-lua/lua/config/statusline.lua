e3f9466c23dc28dfaa4fad9f8839d9f504c945c2
NeutryFD <luisangel20.las@gmail.com>
2025-03-29 14:17:42 +0100

    added statusline to custom nvim

---
+++ nvim/nvim-lua/lua/config/statusline.lua
@@ -0,0 +1,82 @@
+Statusline = {}
+
+
+local modes = {
+  ["n"]  = "NORMAL",
+  ["no"] = "NORMAL",
+  ["v"]  = "VISUAL",
+  ["V"]  = "VISUAL LINE",
+  [""] = "VISUAL BLOCK",
+  ["s"]  = "SELECT",
+  ["S"]  = "SELECT LINE",
+  [""] = "SELECT BLOCK",
+  ["i"]  = "INSERT",
+  ["ic"] = "INSERT",
+  ["R"]  = "REPLACE",
+  ["Rv"] = "VISUAL REPLACE",
+  ["c"]  = "COMMAND",
+  ["cv"] = "VIM EX",
+  ["ce"] = "EX",
+  ["r"]  = "PROMPT",
+  ["rm"] = "MOAR",
+  ["r?"] = "CONFIRM",
+  ["!"]  = "SHELL",
+  ["t"]  = "TERMINAL",
+}
+
+local function mode()
+  local current_mode = vim.api.nvim_get_mode().mode
+  return string.format(" %s ", modes[current_mode]):upper()
+end
+
+local function lineinfo()
+  if vim.bo.filetype == "alpha" then
+    return ""
+  end
+  return " %P %L/%l:%c "
+end
+
+local function filepath()
+  local fpath = vim.fn.fnamemodify(vim.fn.expand "%", ":~:.:h")
+  if fpath == "" or fpath == "." then
+      return " "
+  end
+
+  return string.format(" %%<%s/", fpath)
+end
+
+local function filename()
+  local fname = vim.fn.expand "%:t"
+  if fname == "" then
+      return ""
+  end
+  return fname .. " "
+end
+
+local function filetype()
+  return string.format(" %s ", vim.bo.filetype):upper()
+end
+
+-- Define the statusline function
+Statusline = {}
+
+
+Statusline.active = function()
+  return table.concat {
+--"%#Statusline#",
+    mode(),
+    "%#Normal# ",
+    filepath(),
+    filename(),
+    "%#Normal#",
+    "%=%#StatusLineExtra#",
+    filetype(),
+    lineinfo(),
+  }
+end
+
+function Statusline.inactive()
+  return " %F"
+end
+
+return statusline
