7ae17e53effa0d3b9d6d8d6d1dd9900fa05a9bc9
neutry <luisangel20.las@gmail.com>
2025-05-12 00:00:12 +0200

    added function to use stop/play bottom

--- polybar/scripts/spotify_status.sh
+++ polybar/scripts/spotify_status.sh
@@ -5,11 +5,13 @@ FORMAT="{{ artist }} - {{ title }}"
 SCROLL_SPEED=0.2
 PADDING="    "
 last_metadata=""
+last_status=""
+PARENT_BAR_PID=$(pgrep -f "polybar")
 
 scroll_text() {
     local text="$1$PADDING"
     for ((i = 0; i < ${#text}; i++)); do
-        echo "${text}"
+        echo "$text"
         text="${text:1}${text:0:1}"
         sleep $SCROLL_SPEED
     done
@@ -23,6 +25,13 @@ get_metadata() {
     playerctl --player=$PLAYER metadata --format "$FORMAT" 2>/dev/null
 }
 
+update_hooks() {
+    echo "$1" | while IFS= read -r id
+    do
+        polybar-msg -p "$id" hook spotify-play-pause $2 1>/dev/null 2>&1
+    done
+}
+
 while true; do
     STATUS=$(get_status)
 
@@ -34,8 +43,16 @@ while true; do
 
     if [[ "$metadata" != "$last_metadata" ]]; then
         last_metadata="$metadata"
-        # En lugar de reiniciar la barra, actualiza el módulo
         polybar-msg hook spotify 1 > /dev/null 2>&1
+    fi
+
+    if [[ "$STATUS" != "$last_status" ]]; then
+        last_status="$STATUS"
+        if [[ "$STATUS" == "Playing" ]]; then
+            update_hooks "$PARENT_BAR_PID" 1
+        else
+            update_hooks "$PARENT_BAR_PID" 0
+        fi
     fi
 
     scroll_text "$metadata"
