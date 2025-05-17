2a2c55309c2db3a98d1c4f246266440640eb2b70
neutry <luisangel20.las@gmail.com>
2025-05-17 09:27:27 +0200

    set SO icon automatically

--- polybar/scripts/banner.sh
+++ polybar/scripts/banner.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+LINUX=$(hostnamectl hostname)
+USER=$(echo $USER)
 
-text="| 󱓞  Neutry  |    Arch Linux |    Neovim |   Stay Tiling "
+ARCH_ICON=󰣇
+UBUNTU_ICON=󰕈
+
+if [ $LINUX == "archlinux" ]; then
+
+	ICON=$ARCH_ICON
+fi
+
+text="| 󱓞  $USER  | $ICON  $LINUX |    Neovim |   Stay Tiling "
 
 while true; do
     echo "${text}"
     text="${text:1}${text:0:1}"
     sleep 0.2
 done
-
