0e5e7055fcd4c4830cb59c2fba2c4a072f821d37
neutry <luisangel20.las@gmail.com>
2025-12-13 10:15:27 +0100

    added shortcut for telescope_grep and help

--- nvim/init.lua
+++ nvim/init.lua
@@ -112,11 +112,11 @@ cmp.setup.cmdline(':', {
 })
 
 
-
-require('telescope').setup{
- vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {})
-}
-
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 
 -- disable netrw at the very start of your init.lua
