0b210d3651c838165aa23c4d3e8a73a6ecff5d3f
neutry <luisangel20.las@gmail.com>
2025-12-26 16:04:24 +0100

    add contras to the nvim + split theme from main lua.init

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,24 +13,10 @@ vim.o.shiftwidth = 4
 -- mouse funcionality
 vim.o.mouse = "a"
 
--- theme
-vim.cmd("colorscheme slate")
--- transparencybackgroud 
-vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
-vim.cmd([[highlight NormalNC guibg=NONE ctermbg=NONE]])
-vim.cmd([[highlight EndOfBuffer guibg=NONE ctermbg=NONE]])
-
--- backgroud of visual mode
-vim.cmd([[highlight Visual guibg=#3e4451 guifg=#ffffff]])  -- backgroud grey and text white
--- Change the color of the mode indicator (ModeMsg)
-vim.cmd([[highlight ModeMsg guibg=#3b4252 guifg=#d8dee9]])  -- Dark background and light text (grey colors)
-
-
 -- numbers
 vim.opt.number = true      -- Show absolute line numbers
 vim.opt.relativenumber = true  -- Show relative line numbers
 vim.keymap.set("n", "<leader>n", ":set number! relativenumber!<CR>", { desc = "Toggle line numbers" })
-
 
 -- Hide the Command-line when no use it
 vim.o.cmdheight = 0
@@ -38,6 +24,10 @@ vim.o.cmdheight = 0
 -- statusline
 -- Load the custom statusline from lua/config/statusline.lua
 local Statusline = require('config.statusline')
+
+-- load and configure tokyonight colorscheme
+-- Apply the colorscheme after setup
+require('config.tokyonight')
 
 -- Set the statusline using the active statusline function
 vim.opt.statusline = "%!v:lua.Statusline.active()"
@@ -159,60 +149,6 @@ vim.keymap.set("n", "<leader>b", "<C-w>p", { desc = "Back to previous window" })
 
 vim.opt.termguicolors = true
 
--- theme
----@class tokyonight.Config
----@field on_colors fun(colors: ColorScheme)
----@field on_highlights fun(highlights: tokyonight.Highlights, colors: ColorScheme)
-require("tokyonight").setup({
-  style = "moon", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-  light_style = "day", -- The theme is used when the background is set to light
-  transparent = true, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
-  },
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights tokyonight.Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-
-  cache = true, -- When set to true, the theme will be cached for better performance
-
-  ---@type table<string, boolean|{enabled:boolean}>
-  plugins = {
-    -- enable all plugins when not using lazy.nvim
-    -- set to false to manually enable/disable plugins
-    all = package.loaded.lazy == nil,
-    -- uses your plugin manager to automatically enable needed plugins
-    -- currently only lazy.nvim is supported
-    auto = true,
-    -- add any plugins here that you want to enable
-    -- for all possible plugins, see:
-    --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
-    -- telescope = true,
-  },
-})
-
--- Apply the colorscheme after setup
-vim.cmd.colorscheme("tokyonight")
 
 
 -- terminal
@@ -397,3 +333,76 @@ vim.opt.termguicolors = true
 vim.keymap.set("n", "yy", '"+yy', { noremap = true })
 vim.keymap.set("v", "y", '"+y', { noremap = true })
 
+
+require("cyberdream").setup({
+    -- Set light or dark variant
+    variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+
+    -- Enable transparent background
+    transparent = false,
+
+    -- Reduce the overall saturation of colours for a more muted look
+    saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
+
+    -- Enable italics comments
+    italic_comments = false,
+
+    -- Replace all fillchars with ' ' for the ultimate clean look
+    hide_fillchars = false,
+
+    -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
+    borderless_pickers = false,
+
+    -- Set terminal colors used in `:terminal`
+    terminal_colors = true,
+
+    -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+    cache = false,
+
+    -- Override highlight groups with your own colour values
+    highlights = {
+        -- Highlight groups to override, adding new groups is also possible
+        -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+        -- Example:
+        Comment = { fg = "#696969", bg = "NONE", italic = true },
+
+        -- More examples can be found in `lua/cyberdream/extensions/*.lua`
+    },
+
+    -- Override a highlight group entirely using the built-in colour palette
+    overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+        -- Example:
+        return {
+            Comment = { fg = colors.green, bg = "NONE", italic = true },
+            ["@property"] = { fg = colors.magenta, bold = true },
+        }
+    end,
+
+    -- Override colors
+    colors = {
+        -- For a list of colors see `lua/cyberdream/colours.lua`
+
+        -- Override colors for both light and dark variants
+        bg = "#000000",
+        green = "#00ff00",
+
+        -- If you want to override colors for light or dark variants only, use the following format:
+        dark = {
+            magenta = "#ff00ff",
+            fg = "#eeeeee",
+        },
+        light = {
+            red = "#ff5c57",
+            cyan = "#5ef1ff",
+        },
+    },
+
+    -- Disable or enable colorscheme extensions
+    extensions = {
+        telescope = true,
+        notify = true,
+        mini = true,
+        ...
+    },
+})
--- nvim/install-plugins.sh
+++ nvim/install-plugins.sh
@@ -33,3 +33,6 @@ git clone https://github.com/lukas-reineke/indent-blankline.nvim.git
 
 #copilot
 git clone --depth=1 https://github.com/github/copilot.vim.git
+
+# cyberdream theme
+git clone https://github.com/scottmckendry/cyberdream.nvim.git
