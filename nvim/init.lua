a6d737f41a0b11d50b4d81d7cbaa9024b2f5bf22
neutry <luisangel20.las@gmail.com>
2025-12-26 19:36:30 +0100

    update nvim-tree plugin colors + added feature to automatically open created file

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,7 +22,7 @@ vim.opt.relativenumber = true  -- Show relative line numbers
 -- Hide the Command-line when no use it
 vim.o.cmdheight = 0
 
--- disable netrw at the very start of your init.lua
+-- disable netrw at the very start of your init.lua reccomended by nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -64,7 +64,6 @@ vim.api.nvim_exec([[
 -- Load plugins and configurations
 require('config.tokyonight')
 require('config.markdown-renderer')
-require('config.cyberdream')
 require('config.lazygit-setup')
 require('config.tree')
 require('config.terminal')
