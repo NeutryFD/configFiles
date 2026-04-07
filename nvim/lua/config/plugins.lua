bcae77ab1f39e1f1df252f23c4d355345a93b769
neutry <luisangel20.las@gmail.com>
2026-04-07 23:35:44 +0200

    added vim.pack.add to nvim  config

---
+++ nvim/lua/config/plugins.lua
@@ -0,0 +1,49 @@
+vim.pack.add({
+	--- Clone nvim-cmp
+	'https://github.com/hrsh7th/nvim-cmp',
+
+	-- Cline depencies
+	'https://github.com/hrsh7th/cmp-nvim-lsp',
+	'https://github.com/hrsh7th/cmp-buffer',
+	'https://github.com/hrsh7th/cmp-path',
+	'https://github.com/hrsh7th/cmp-cmdline',
+	'https://github.com/L3MON4D3/LuaSnip',
+	'https://github.com/saadparwaiz1/cmp_luasnip',
+
+	---- Clone Telescope
+	'https://github.com/nvim-telescope/telescope.nvim.git',
+
+	---- Depencies for Telescope
+	'https://github.com/nvim-lua/plenary.nvim.git',
+	
+	-- Nvimtree
+	'https://github.com/nvim-tree/nvim-tree.lua.git',
+
+	-- Use HackNerdFont in Nvim
+	'https://github.com/nvim-tree/nvim-web-devicons.git',
+
+	----theme
+	'https://github.com/catppuccin/nvim.git',
+	'https://github.com/folke/tokyonight.nvim.git',
+	
+	----indentation lines
+	'https://github.com/lukas-reineke/indent-blankline.nvim.git',
+	
+	----copilot
+	'https://github.com/github/copilot.vim.git',
+	
+	----Markdown renderer
+	'https://github.com/MeanderingProgrammer/render-markdown.nvim.git',
+	
+	----Terraform syntax highlighting
+	'https://github.com/hashivim/vim-terraform.git',
+	
+	----Notify
+	'https://github.com/rcarriga/nvim-notify.git',
+	
+	----Noice 
+	'https://github.com/folke/noice.nvim.git',
+	
+	--UI components (floating windows, input, select)
+	'https://github.com/MunifTanjim/nui.nvim.git'
+})
