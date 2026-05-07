a6c2cd2c56eef997c107761419dfaf283cd8f1aa
neutry <luisangel20.las@gmail.com>
2026-05-07 22:42:29 +0200

    nvim: change icon for YAMLs files

--- nvim/lua/config/nvim-web-devicons.lua
+++ nvim/lua/config/nvim-web-devicons.lua
@@ -1,12 +1,12 @@
 require("nvim-web-devicons").setup({
   override = {
     yaml = {
-      icon = "  ", -- you can keep or change this
+      icon = "", -- you can keep or change this
       color = "#6d8086", -- your custom color
       name = "Yaml",
     },
     yml = {
-      icon = "  ",
+      icon = "",
       color = "#6d8086",
       name = "Yaml",
     },
