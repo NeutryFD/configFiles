77055e29cc48cc7b1945fb01686208914b0d74ba
neutry <luisangel20.las@gmail.com>
2026-06-09 12:08:58 +0200

    nvim: configure tf.nvim for interactive terraform state management

--- nvim/lua/config/terraform.lua
+++ nvim/lua/config/terraform.lua
@@ -1,2 +1,16 @@
 vim.g.terraform_fmt_on_save = 1
 vim.g.terraform_align = 1
+
+require("tf").setup({
+  filetypes = { "terraform", "tf", "terraform-vars", "tfvars", "hcl" },
+  state = {
+    filter = { case_sensitive = true },
+    detail = { folds = true, foldmethod = "syntax" },
+    window = {
+      mode = "float",
+      split = { position = "botright", size = 80 },
+      float = { width = 0.7, height = 0.8 },
+      focus = true,
+    },
+  },
+})
